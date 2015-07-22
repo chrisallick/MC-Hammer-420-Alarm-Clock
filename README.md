bd3dc66a068f73746da7903c41518307cd5c79c4
Chris Allick <chrisallick@gmail.com>
2015-07-22 09:55:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hammer Time 420 Alarm Clock
+A Chrome Extension where MC Hammer dances on your screen and you can't click anything till he is done.
+
+How to use:
+
+1. Download this repo.
+2. Open "Extensions" page in the Chrome browser.
+3. Enable "Developer Mode" by checking the box (top right).
+4. Click "Load unpacked extension..."
+5. Select the folder of this repository you downloaded.
+6. Walk away from your friend's computer giggling
---
+++ main.js
@@ -0,0 +1,62 @@
+function play() {
+    $("body").prepend(img);
+
+    var url = chrome.extension.getURL('stop.m4a');
+    var myAudio = new Audio();
+    myAudio.src = url;
+    myAudio.play();
+
+    setTimeout(function(){
+        $(img).css({
+            display: "block"
+        }).animate({
+            left: 0
+        }, function(){
+            $(this).addClass("move");
+        });
+    }, 500);
+
+    $("a").each(function(){
+        $(this).attr("href","").click(function(){
+            return false;
+        });
+    });
+
+    setTimeout(function(){
+        location.reload();
+    }, 33*1000);
+}
+
+function hammertime() {
+    var url = chrome.extension.getURL('hammer.gif');
+    
+    img = document.createElement("img");
+    
+    $(img).attr("src", url);
+    $(img).addClass("hammertime");
+    $(img).load(function(){
+        $(img).css({
+            bottom: 0,
+            left: -(img.width/2),
+            position: "fixed",
+            zIndex: 1000000,
+            width: img.width/2,
+            height: img.height/2,
+            display: "none"
+        });
+
+        play();
+    });
+}
+
+var img;
+$(document).ready(function() {
+    var now = new Date();
+    var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 20, 0, 0) - now;
+    if (millisTill10 < 0) {
+        millisTill10 += 86400000;
+    }
+    setTimeout(function() {
+        hammertime();
+    }, millisTill10);
+});
